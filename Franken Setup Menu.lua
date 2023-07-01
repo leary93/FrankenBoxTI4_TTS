1fb66b0f4e2fc77d0c0f9ebfc20f5c383bbef0a1
leary93 <leroyodk@gmail.com>
2023-07-01 16:28:17 +0200

    Add files via upload

--- Franken Setup Menu.lua
+++ Franken Setup Menu.lua
@@ -5,9 +5,13 @@ local _config = false
 
 local DEFAULT_CONFIG = {
     Blue_count = 4,
+    Red_count = 2,
+    Faction_tech_count = 4,
+    Faction_ability_count = 5,
     Powered_draft = false,
     Speaker_order_draft = false,
     Spage_tech = false,
+    Include_DS = true,
     Ban_negatives = false,
     Ban_bad = false,
     Replace_components = false,
@@ -37,33 +41,41 @@ local _zoneHelper = getHelperClient('TI4_ZONE_HELPER')
 
 function onLoad(saveState)
     _config = DEFAULT_CONFIG
-    if saveState and string.len(saveState) > 0 then
-        _config = JSON.decode(saveState) or _config
-    end
 
     Wait.frames(_createUI, 2)
     scheduleUpdateUiFromConfig(4)
 
-    self.setDescription([[
+    local description = [[
         Use this tool to pre-curate most of the Franken setup. 
         Click 'Gather Draft Items' only if the Franken Box is out on the table.
 
-        Extra Blue Tile: Adds 1 Extra blue tile to the draft bags. Players discard 1 blue tile before building their slice.
+        Amount of Blue/Red tiles and Faction tech/abilities: Amount dealt to each players' bag
 
         Draft Speaker Order: Adds the numbers 1-6 to the draft. Players can only draft 1 of these. If not playing 6 players, make sure to remove the other numbers before Building bags. 
 
-        Powered: This adds 1 extra faction tech and ability to each players' draft bags. Players may keep 3 faction techs and 4 faction abilities, instead of 2/3.
+        Spage tech variant: Replaces normal starting tech. Instead of drafting 2 starting tech components and discarding 1, you draft 2 slightly less powerful starting techs and discard none.
+
+        Include DS components: If the DS box is loaded in for it's content pack (planets, explorations, relics) but you do not want to use it for franken components, leave this box unchecked. Otherwise, check this box. 
 
         Ban negatives: This removes negative abilities, like Fragile or Mitosis.
 
         Ban low-value: This removes components with low to near-0 value, like Sardakk Starting Tech or Shield Paling.
 
-        Replace Components: This will look for a bag named 'Replacing Components' to add alternative versions of components to the draft. Make sure there is only 1 such bag, and that each component to replace is named exactly as the original.
-
-        Include custom Franken pack: This is a custom add-on with loose components that did not fit in any DS factions but can be used for Franken Draft.
+        Include custom bans: This allows you to include custom bans. Right click on the setup tool to open the context menu and toggle the custom ban input there. Once you're happy with your custom bans, toggle the input back off and check this box.
 
         **Pillage is auto-removed (you may put it back in from the 'Removed' bag)
-    ]])
+
+        ----
+
+        2 pre-sets are adopted in the Context menu as well: 
+
+        6p Yvon Soup - this is the standard powered DS setup with many bans to weak and negative components.
+        4p Jefferson - this is the standard non-DS powered setup with custom bans to negative and overwhelming components (can edit these if u want with custom ban input)
+    ]]
+
+    self.setDescription(description)
+
+    printToAll(description, "Yellow")
 
     local pos = {
         x = 40,
@@ -73,6 +85,9 @@ function onLoad(saveState)
     self.setPositionSmooth(pos, false, false)
 
     self.addContextMenuItem('Toggle Ban input', toggleCustomBans)
+    self.addContextMenuItem('Set 6p Yvon Soup', setYvonSoup)
+    self.addContextMenuItem('Set 4p Jefferson', setJeffersonSoup)
+
 end
 
 function onSave()
@@ -87,7 +102,7 @@ end
 
 function _createUI()
     local scale = self.getScale()
-    local uiScale = (4 / scale.x) .. ' ' .. (4 / scale.z) .. ' ' .. (1 / scale.y)
+    local uiScale = (3.3 / scale.x) .. ' ' .. (3.3 / scale.z) .. ' ' .. (1 / scale.y)
 
     local function text(class, text)
         return {
@@ -99,7 +114,7 @@ function _createUI()
         }
     end
 
-    local function toggle(id, text, isOn, tooltip)
+    local function toggle(id, text, isOn)
         return {
             tag = 'Toggle',
             attributes = {
@@ -107,7 +122,7 @@ function _createUI()
                 isOn = isOn or nil
             },
             value = text,
-            tooltip = tooltip
+            
         }
     end
     local function toggleGroup(idPrefix, values)
@@ -153,7 +168,7 @@ function _createUI()
                     attributes = {
                         fontSize = 16,
                         color = 'White',
-                        alignment = 'MiddleRight',
+                        alignment = 'MiddleLeft',
                         minWidth = 110,
                         minHeight = height,
                     },
@@ -169,7 +184,7 @@ function _createUI()
                         onValueChanged = 'onSliderValueChanged',
                         minWidth = 100,
                         minHeight = height,
-                        rectAlignment = 'MiddleCenter'
+                        rectAlignment = 'MiddleRight'
                     },
                     value = minValue,
                 },
@@ -179,7 +194,7 @@ function _createUI()
                         id = id .. 'Value',
                         fontSize = 16,
                         color = 'White',
-                        alignment = 'MiddleLeft',
+                        alignment = 'MiddleRight',
                         horizontalOverflow = 'Overflow',
                         verticalOverflow = 'Overflow',
                         minWidth = 20,
@@ -277,8 +292,8 @@ function _createUI()
         attributes = {
             position = '0 0 2',
             rotation = '0 180 0',
-            width = 300,
-            height = 500,
+            width = 360,
+            height = 600,
             scale = uiScale,
         },
         children = {
@@ -292,22 +307,27 @@ function _createUI()
 
                     spacer(),
 
-                    slider('Amount of blue tiles', 'Blue_count', 2, 8),
+                    slider('Amount of blue tiles', 'Blue_count', 2, 8, 'Set amount of blue tiles in each players bag'),
+                    slider('Amount of red tiles', 'Red_count', 1, 4, 'Set amount of red tiles in each players bag'),
+                    slider('Amount of faction abilities', 'Faction_ability', 2, 8, 'Set amount of faction abilities in each players bag'),
+                    slider('Amount of faction technologies', 'Faction_tech', 2, 6, 'Set amount of faction technologies in each players bag'),
 
-                    toggle('Speaker_order_draft','Draft Speaker Order', ''),
-                    toggle('Powered_draft','Powered (+1 faction tech & ability)', ''),
-                    toggle('Ban_negatives','Remove negative abilities', ''),
-                    toggle('Ban_bad','Remove low value components', ''),
-                    toggle('Include_Custom_Bans', 'Include custom bans', 'Toggle the input for this in the context menu (right click this menu).'),
-                    toggle('Replace_components','Replace components', ''),
-                    toggle('Include_Franken_Pack', 'Include custom Franken pack', '')
+                    toggle('Speaker_order_draft','Draft Speaker Order'),
+                    --toggle('Powered_draft','Powered (+1 faction tech & ability)'),
+                    toggle('Spage_tech', 'Spage Tech variant'),
+                    toggle('Include_DS', 'Include DS components'),
+                    toggle('Ban_negatives','Remove negative abilities'),
+                    toggle('Ban_bad','Remove low value components'),
+                    toggle('Include_Custom_Bans', 'Include custom bans'),
+                    --toggle('Replace_components','Replace components'),
+                    --toggle('Include_Franken_Pack', 'Include custom Franken pack')
                 }
             },
             {
                 tag = 'Panel',
                 attributes = {
-                    height = 210,
-                    padding = '10 10 10 150',
+                    height = 150,
+                    padding = '10 10 0 110',
                     rectAlignment = 'LowerCenter',
                 },
                 children = {
@@ -315,7 +335,7 @@ function _createUI()
                         tag = 'Button',
                         attributes = {
                             id = 'gatherDraft',
-                            fontSize = 24,
+                            fontSize = 16,
                         },
                         value = '1. Gather Draft Items'
                     },
@@ -324,8 +344,8 @@ function _createUI()
             {
                 tag = 'Panel',
                 attributes = {
-                    height = 140,
-                    padding = '10 10 10 80',
+                    height = 100,
+                    padding = '10 10 0 60',
                     rectAlignment = 'LowerCenter',
                 },
                 children = {
@@ -333,7 +353,7 @@ function _createUI()
                         tag = 'Button',
                         attributes = {
                             id = 'buildDraft',
-                            fontSize = 24,
+                            fontSize = 16,
                         },
                         value = '2. Build Draft Bags'
                     },
@@ -342,8 +362,8 @@ function _createUI()
             {
                 tag = 'Panel',
                 attributes = {
-                    height = 70,
-                    padding = '10 10 10 10',
+                    height = 50,
+                    padding = '10 10 0 10',
                     rectAlignment = 'LowerCenter',
                 },
                 children = {
@@ -351,7 +371,7 @@ function _createUI()
                         tag = 'Button',
                         attributes = {
                             id = 'finishDraft',
-                            fontSize = 24,
+                            fontSize = 16,
                         },
                         value = '3. Remove Draft Bags'
                     },
@@ -364,24 +384,32 @@ function _createUI()
         attributes = {
             position = '0 0 -22',
             rotation = '0 0 0',
-            width = 300,
-            height = 500,
+            width = 360,
+            height = 600,
             padding = '20 20 20 20',
             scale = uiScale,
-            color = '#000000e0'
+            color = '#000000e0',
         },
         children = {
         }
     }
 
-    self.UI.setXmlTable({ defaults, top, bottom })
+    self.UI.setXmlTable({ defaults, top})
 end
 
 function updateUiFromConfig()
     self.UI.setAttribute('Blue_count','value', _config.Blue_count)
     self.UI.setValue('Blue_countValue', _config.Blue_count)
+    self.UI.setAttribute('Red_count','value', _config.Red_count)
+    self.UI.setValue('Red_countValue', _config.Red_count)
+    self.UI.setAttribute('Faction_ability','value', _config.Faction_ability_count)
+    self.UI.setValue('Faction_abilityValue', _config.Faction_ability_count)
+    self.UI.setAttribute('Faction_tech','value', _config.Faction_tech_count)
+    self.UI.setValue('Faction_techValue', _config.Faction_tech_count)
     self.UI.setAttribute('Speaker_order_draft', 'isOn', _config.Speaker_order_draft)
-    self.UI.setAttribute('Powered_draft', 'isOn', _config.Powered_draft)
+    -- self.UI.setAttribute('Powered_draft', 'isOn', _config.Powered_draft)
+    self.UI.setAttribute('Spage_tech', 'isOn', _config.Spage_tech)
+    self.UI.setAttribute('Include_DS', 'isOn', _config.Include_DS)
     self.UI.setAttribute('Ban_negatives', 'isOn', _config.Ban_negatives)
     self.UI.setAttribute('Ban_bad', 'isOn', _config.Ban_bad)
     self.UI.setAttribute('Replace_components', 'isOn', _config.Replace_components)
@@ -434,12 +462,12 @@ function showCustomBans()
             function_owner = self,
             label          = hint,
             alignment      = 2, -- left
-            position       = { x = 0, y = 0, z = 0 },
+            position       = { x = 0, y = -0.5, z = 0 },
             rotation       = { x = 0, y = 180, z = 180 },
             scale          = { x = 1, y = 1, z = 1 },
             width          = 5500,
             height         = 9000,
-            font_size      = 300,
+            font_size      = 200,
             tooltip        = hint,
             value          = customBanInput, -- Show label as "hint"
         })
@@ -459,8 +487,10 @@ function setCustomBans()
 
     }
 
-    for component in string.gmatch(customBanInput, "([^%s;%s]+)") do
-        COMPONENTS_TO_BAN_CUSTOM[component] = {}
+    for component in string.gmatch(customBanInput, "([^;]+)") do
+        local str = string.gsub(component, "^[%s]*", '')
+        str = string.gsub(str, "[%s]*$", '')
+        COMPONENTS_TO_BAN_CUSTOM[str] = {}
     end
 
     _frankenBox.setCustomBans(COMPONENTS_TO_BAN_CUSTOM)
@@ -492,6 +522,10 @@ function onToggleValueChanged(player, value, id)
         _config.Speaker_order_draft = valueAsBool
     elseif id == 'Powered_draft' then
         _config.Powered_draft = valueAsBool
+    elseif id == 'Spage_tech' then
+        _config.Spage_tech = valueAsBool
+    elseif id == 'Include_DS' then
+        _config.Include_DS = valueAsBool
     elseif id == 'Ban_negatives' then
         _config.Ban_negatives = valueAsBool
     elseif id == 'Ban_bad' then
@@ -513,8 +547,55 @@ function onSliderValueChanged(player, value, id)
     value = tonumber(value)
     if id == 'Blue_count' then
         _config.Blue_count = value
+    elseif id == 'Red_count' then
+        _config.Red_count = value
+    elseif id == 'Faction_ability' then
+        _config.Faction_ability_count = value
+    elseif id == 'Faction_tech' then
+        _config.Faction_tech_count = value
     end
     scheduleUpdateUiFromConfig()
+end
+
+------
+
+function setYvonSoup()
+    -- Pre-set the tool to yvon soup settings
+    _config.Blue_count = 4
+    _config.Red_count = 2
+    _config.Faction_ability_count = 5
+    _config.Faction_tech_count = 4
+    _config.Include_DS = true
+    _config.Speaker_order_draft = true
+    _config.Ban_negatives = true
+    _config.Ban_bad = true
+    _config.Include_Custom_Bans = false
+
+    scheduleUpdateUiFromConfig(2)
+end
+
+function setJeffersonSoup()
+    -- Pre-set the tool to jefferson soup settings
+    _config.Blue_count = 5
+    _config.Red_count = 3
+    _config.Faction_ability_count = 5
+    _config.Faction_tech_count = 4
+    _config.Include_DS = false
+    _config.Speaker_order_draft = false
+    _config.Ban_negatives = false
+    _config.Ban_bad = false
+    _config.Include_Custom_Bans = true
+
+    customBanInput = [[
+        Winnu Fleet ; Titans Fleet ; Letani Warrior II ; Floating Factory II ; Quantum Datahub Node ;
+        Hubris ; Propagation ; Fragile ; Mitosis ; Pillage ;
+        Z'ue ; Z'eu Ω ; Brother Milor ; M'aban ; Il Na Viroset ; Scepter of Dominion ;
+        Mathis Mathinus ; Xxekir Grom ; Xxekir Grom Ω ;
+        Shield Paling ; Iconoclast ; Ghosts of Creuss Tile ; 
+        Arborec Starting Tech ; Xxcha Starting Tech ; Sardakk Starting Tech ;
+    ]]
+
+    scheduleUpdateUiFromConfig(2)
 end
 
 ------
